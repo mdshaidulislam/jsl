c4092858baab277b2b2ecdc89480fa19bb974716
mdshaidulislam <shaidulislam2013@gmail.com>
2024-10-29 11:01:14 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// let x ={
+//     name: "Zahid",
+//     age: 21
+    
+// }
+// console.log(x.age);
+
+// let{name,age}={
+//     name:"Zahid",
+//     age:21
+    
+// }
+// console.log(name);
+let teamMembers = ['Zahid','Shaidul','Mazz']
+teamMembers.pop()
+// teamMembers.unshift('Mamun')
+// teamMembers.push('Khan')
+// teamMembers[3]='Ahmed'
+console.log(teamMembers);
+
+console.log(teamMembers[teamMembers.length-1]);
+
+console.log(teamMembers.join(' $ '));
+
+console.log(teamMembers.length-1);
+
+
+
+
