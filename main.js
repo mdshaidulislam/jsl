42a916852429e050208bf8dae3573b5ee9e9fb7e
mdshaidulislam <shaidulislam2013@gmail.com>
2024-10-29 11:07:03 +0600

    secend commt

--- main.js
+++ main.js
@@ -1,164 +1,164 @@
-// let myVariableName = 'string' 
-// // "What's your name?" `j`
-// const myBool = true // false
+let myVariableName = 'string' 
+// "What's your name?" `j`
+const myBool = true // false
 
-// const dhaka = 'Capital'
-
-
-// const myNumber = 30
-// const myNumber2 = 33
-
-// if(myNumber == myNumber2){
-//     console.log('Hello');
-// } else {
-//     console.log('Sorry')
-// }
+const dhaka = 'Capital'
 
 
+const myNumber = 30
+const myNumber2 = 33
 
-// if( 30 < 20 ){
-//     console.log('Hello');
-// } else if( 30 < 35 ){
-//     const x = 10
-//     const y = 12
-//     console.log(x*y);
-// } else {
-//     console.log('Sorry');
-// }
-
-// const marks = prompt('Enter your name')
-
-// if(marks >= 33 && marks <= 40){
-//     console.log('Grade: D');
-// } else if(marks >= 41 && marks <= 50){
-//     console.log('Grade: C');
-// } else if(marks >= 51 && marks <= 60){
-//     console.log("Grade B");
-// } else if(marks < 0 || marks > 100){
-//     console.log('Sorry, give a valid number');
-// } else{
-//     console.log('Fail');
-// }
+if(myNumber == myNumber2){
+    console.log('Hello');
+} else {
+    console.log('Sorry')
+}
 
 
-// const gender = prompt('Enter your gender')
-// const age = prompt('Enter your age')
-// const home = prompt('Dhaka')
-// const haveJob = true 
 
-// if(gender === 'Male'){
-//    if(age >= 25){
-//     if(home === 'Dhaka'){
-//         console.log('Welcome');
-//     } else{
-//         console.log('Sorry. We are looking for someone from Dhaka.');
-//     }
-//    } else{
-//     console.log('Sorry. Your age did not match.');
-//    }
-// } else {
-//     console.log('Sorry. We are looking for male candidate.');
-// }
+if( 30 < 20 ){
+    console.log('Hello');
+} else if( 30 < 35 ){
+    const x = 10
+    const y = 12
+    console.log(x*y);
+} else {
+    console.log('Sorry');
+}
 
+const marks = prompt('Enter your name')
 
-// for(let i = 0; i < 5; i++){
-//     console.log(i);
-// }
-
-// const myArray = ['Alamin', 20, true]
-// const myString = 'Dhaka'
-
-// console.log(animalList.length);
-// console.log(animalList);
-// console.log(animalList[0]);
-// console.log(animalList[1]);
-// console.log(animalList[2]);
-// console.log(animalList[3]);
-
-// for(let i = 0; i < animalList.length; i++){
-//     console.log(animalList[i]);
-// }
+if(marks >= 33 && marks <= 40){
+    console.log('Grade: D');
+} else if(marks >= 41 && marks <= 50){
+    console.log('Grade: C');
+} else if(marks >= 51 && marks <= 60){
+    console.log("Grade B");
+} else if(marks < 0 || marks > 100){
+    console.log('Sorry, give a valid number');
+} else{
+    console.log('Fail');
+}
 
 
-// animalList.forEach()
+const gender = prompt('Enter your gender')
+const age = prompt('Enter your age')
+const home = prompt('Dhaka')
+const haveJob = true 
+
+if(gender === 'Male'){
+   if(age >= 25){
+    if(home === 'Dhaka'){
+        console.log('Welcome');
+    } else{
+        console.log('Sorry. We are looking for someone from Dhaka.');
+    }
+   } else{
+    console.log('Sorry. Your age did not match.');
+   }
+} else {
+    console.log('Sorry. We are looking for male candidate.');
+}
 
 
-// function myFunction(){
-//     console.log(animal)
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
 
-// } 
+const myArray = ['Alamin', 20, true]
+const myString = 'Dhaka'
+
+console.log(animalList.length);
+console.log(animalList);
+console.log(animalList[0]);
+console.log(animalList[1]);
+console.log(animalList[2]);
+console.log(animalList[3]);
+
+for(let i = 0; i < animalList.length; i++){
+    console.log(animalList[i]);
+}
+
+
+animalList.forEach()
+
+
+function myFunction(){
+    console.log(animal)
+
+} 
     
-// const animalFunction = () => {  }
+const animalFunction = () => {  }
 
 
 
-//     // myFunction()
+    // myFunction()
     
-// const animalList = ['Tiger', 'Cat', 'Dog', 'Lion', 'Deer', 'Wolf']
+const animalList = ['Tiger', 'Cat', 'Dog', 'Lion', 'Deer', 'Wolf']
+animalList.forEach(animal => {console.log(animal)})
 // animalList.forEach(animal => {console.log(animal)})
-// // animalList.forEach(animal => {console.log(animal)})
-// console.log("***********************");
-// animalList.map(animal2 => {console.log(animal2)})
+console.log("***********************");
+animalList.map(animal2 => {console.log(animal2)})
  
 
 
-// let team = ["Rakib","Akib","Mamun","khan","Zahid","Islam",]
-//  console.log(team);
+let team = ["Rakib","Akib","Mamun","khan","Zahid","Islam",]
+ console.log(team);
 
-// for (let i = 0; i < team.length; i++) {
-//     console.log(team[i]);  
-// }
-//  team.map(team =>{console.log(team);
-//  })
-//  team.forEach(team =>{console.log(team);
-//  })
-
-
-
-// let utterance = new SpeechSynthesisUtterance(team);
-// speechSynthesis.speak(utterance);
+for (let i = 0; i < team.length; i++) {
+    console.log(team[i]);  
+}
+ team.map(team =>{console.log(team);
+ })
+ team.forEach(team =>{console.log(team);
+ })
 
 
-// var name = ("Mamun Khan");
-// var name = ("Shaidul Islam");
-// console.log(name);
 
-// let name = ("Md.Mamun Khan");
-// // let name = ("Md.Khan");
-// name = ("Mr.Khan");
-// console.log(name);
-
-// let x =643
-// let y =435
-// let z =(x+y)
-// let text = ("Amar Kachhe")
-// console.log(text +" " + z + " " + "Taka");
-
-// let Marks = 1
-
-// if (Marks >100 || Marks <=1) {
-//     console.log("Tumi Error");  
-// }
-// else if (Marks >=33 && Marks <50) {
-//     console.log("Tumi D");  
-// }
-// else if (Marks >=50 && Marks <60) {
-//     console.log("Tumi C");  
-// }
-// else if (Marks >=60 && Marks <70) {
-//     console.log("Tumi B");  
-// }
-// else if (Marks >=70 && Marks <80) {
-//     console.log("Tumi A");  
-// }
-// else if (Marks >=80 && Marks <=100) {
-//     console.log("Tumi A+");  
-// }
-// else{
-//     console.log("Fail"); 
-// }
+let utterance = new SpeechSynthesisUtterance(team);
+speechSynthesis.speak(utterance);
 
 
-// 
+var name = ("Mamun Khan");
+var name = ("Shaidul Islam");
+console.log(name);
+
+let name = ("Md.Mamun Khan");
+// let name = ("Md.Khan");
+name = ("Mr.Khan");
+console.log(name);
+
+let x =643
+let y =435
+let z =(x+y)
+let text = ("Amar Kachhe")
+console.log(text +" " + z + " " + "Taka");
+
+let Marks = 1
+
+if (Marks >100 || Marks <=1) {
+    console.log("Tumi Error");  
+}
+else if (Marks >=33 && Marks <50) {
+    console.log("Tumi D");  
+}
+else if (Marks >=50 && Marks <60) {
+    console.log("Tumi C");  
+}
+else if (Marks >=60 && Marks <70) {
+    console.log("Tumi B");  
+}
+else if (Marks >=70 && Marks <80) {
+    console.log("Tumi A");  
+}
+else if (Marks >=80 && Marks <=100) {
+    console.log("Tumi A+");  
+}
+else{
+    console.log("Fail"); 
+}
+
+
+// jxHjxhajh
 
 
